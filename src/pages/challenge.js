e37d89cf5df13eec13143b36982681fd8e17605b
Duong-NVH <devodoi123@gmail.com>
2020-10-29 15:05:25 +0700

    fix warning

--- src/pages/challenge.js
+++ src/pages/challenge.js
@@ -1,11 +1,7 @@
 import React from "react";
-import MainLayout from "../layouts/MainLayout";
+
 function Challenge() {
-  return (
-    <MainLayout>
-      <h1>this is home page</h1>
-    </MainLayout>
-  );
+  return <div>Challenge</div>;
 }
 
 export default Challenge;
