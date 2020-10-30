acd98d9c04b112f1874e731bd13a0f52c243a9d3
Duong-NVH <devodoi123@gmail.com>
2020-10-30 09:50:40 +0700

    mobile signup

--- src/components/AuthPopup/Signup.js
+++ src/components/AuthPopup/Signup.js
@@ -2,72 +2,95 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import styles from "./styles";
 import PropTypes from "prop-types";
-import { useTranslation } from "react-i18next";
-import { IconButton, Button, Typography, Link, Divider, TextField } from "@material-ui/core/";
 import { CloseIcon, FbIcon, GgIcon } from "../../../public/images/svg";
+import { IconButton, Button, Typography, Link, Divider, TextField, useTheme, useMediaQuery } from "@material-ui/core/";
+import { useTranslation } from "react-i18next";
+import clsx from "clsx";
 Signup.propTypes = {
   changeForm: PropTypes.func,
   close: PropTypes.func,
 };
 function Signup(props) {
-  const classes = useStyles();
   const { changeForm, close } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
   const { t: getText } = useTranslation();
   return (
-    <div className={`${classes.container} ${classes.center}`}>
-      <div className={classes.header}>
-        <Typography className={classes.title}>{getText("TXT_SIGNUP")}</Typography>
-        <IconButton classes={classes.center}>
-          <CloseIcon width="19" height="19" onClick={close} fill="#7B93A5" />
-        </IconButton>
-      </div>
+    <div className={clsx(classes.container, classes.center, isMobile ? classes.mcontainer : classes.dcontainer)}>
+      {isMobile ? (
+        <div className={classes.mheader}>
+          <IconButton>
+            <CloseIcon width="12" height="12" onClick={close} fill="#001a39" />
+          </IconButton>
+          <Typography className={clsx(classes.title, classes.mtitle)}>{getText("TXT_SIGNUP")}</Typography>
+        </div>
+      ) : (
+        <div className={classes.header}>
+          <Typography className={classes.title}>{getText("TXT_SIGNUP")}</Typography>
+          <IconButton classes={classes.center}>
+            <CloseIcon width="19" height="19" onClick={close} fill="#7B93A5" />
+          </IconButton>
+        </div>
+      )}
       <Divider className={classes.divider} />
-      <TextField
-        id="email"
-        label={<Typography className={classes.inputLabel}>{getText("L_EMAIL")}</Typography>}
-        className={classes.textInput}
-        inputProps={{ style: styles.textOfInput }}
-        InputProps={{ disableUnderline: true }}
-      />
-      <TextField
-        id="password"
-        label={<Typography className={classes.inputLabel}>{getText("L_PASSWORD")}</Typography>}
-        className={classes.textInput}
-        inputProps={{ style: styles.textOfInput }}
-        InputProps={{ disableUnderline: true }}
-        type="password"
-      />
-      <TextField
-        id="password2"
-        label={<Typography className={classes.inputLabel}>{getText("L_PASSWORD2")}</Typography>}
-        className={classes.textInput}
-        inputProps={{ style: styles.textOfInput }}
-        InputProps={{ disableUnderline: true }}
-        type="password"
-      />
-      <Button className={classes.loginBtn}>{getText("TXT_SIGNUP")}</Button>
-      <Typography className={classes.sclText}>{getText("TXT_LOGIN_SOCIAL_MEDIA")}</Typography>
-      <div className={classes.sclBtnContainer}>
-        <Button className={`${classes.sclBtn} ${classes.fbBtn} ${classes.center}`}>
-          <div className={classes.fsclBtnText}>
-            <FbIcon width="9" height="18" />
-            {` Facebook`}
+      <div className={isMobile ? classes.mcontent : classes.content}>
+        <div className={classes.form}>
+          <TextField
+            id="email"
+            label={<Typography className={classes.inputLabel}>{getText("L_EMAIL")}</Typography>}
+            className={clsx(classes.textInput, isMobile && classes.mtextInput)}
+            inputProps={{ style: styles.textOfInput }}
+            InputProps={{ disableUnderline: true }}
+            type="email"
+            fullWidth
+          />
+          <TextField
+            id="password"
+            label={<Typography className={classes.inputLabel}>{getText("L_PASSWORD")}</Typography>}
+            className={clsx(classes.textInput, isMobile && classes.mtextInput)}
+            inputProps={{ style: styles.textOfInput }}
+            InputProps={{ disableUnderline: true }}
+            type="password"
+            fullWidth
+          />
+          <TextField
+            id="password2"
+            label={<Typography className={classes.inputLabel}>{getText("L_PASSWORD2")}</Typography>}
+            className={clsx(classes.textInput, isMobile && classes.mtextInput)}
+            inputProps={{ style: styles.textOfInput }}
+            InputProps={{ disableUnderline: true }}
+            type="password"
+            fullWidth
+          />
+          <Button className={classes.loginBtn} fullWidth>
+            {getText("TXT_SIGNUP")}
+          </Button>
+        </div>
+        <div className={classes.bottom}>
+          <Typography className={classes.sclText}>{getText("TXT_LOGIN_SOCIAL_MEDIA")}</Typography>
+          <div className={classes.sclBtnContainer}>
+            <Button className={clsx(classes.sclBtn, classes.fbBtn, classes.center)}>
+              <div className={classes.fsclBtnText}>
+                <FbIcon width="9" height="18" />
+                {` Facebook`}
+              </div>
+            </Button>
+            <Button className={clsx(classes.sclBtn, classes.ggBtn, classes.center)}>
+              <div className={classes.gsclBtnText}>
+                <GgIcon width="16" height="16" />
+                {` Google`}
+              </div>
+            </Button>
           </div>
-        </Button>
-        <Button className={`${classes.sclBtn} ${classes.ggBtn} ${classes.center}`}>
-          <div className={classes.gsclBtnText}>
-            <GgIcon width="16" height="16" />
-            {` Google`}
-          </div>
-        </Button>
+          <Typography className={classes.footText}>
+            {getText("TXT_HAVE_ACC")}
+            <Link className={classes.resLink} onClick={changeForm}>
+              {getText("TXT_LOGIN")}
+            </Link>
+          </Typography>
+        </div>
       </div>
-      <Typography className={classes.footText}>
-        {getText("TXT_HAVE_ACC")}
-
-        <Link className={classes.resLink} onClick={changeForm}>
-          {getText("TXT_LOGIN")}
-        </Link>
-      </Typography>
     </div>
   );
 }
