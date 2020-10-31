b3feb3c006cf9dfa9324954b43052c55a99bb819
Duong-NVH <devodoi123@gmail.com>
2020-10-31 16:26:19 +0700

    remove from pages/index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 // import { withAuthSync } from "../utils/auth";
 import MainLayout from "../layouts/MainLayout";
-import AuthDialog from "../../src/components/AuthDialog";
 const Home = () => (
   <MainLayout>
     <h1>this is home page</h1>
-    <AuthDialog isOpen={true} />
   </MainLayout>
 );
 
