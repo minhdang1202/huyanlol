ebcb7eb334a233671269a352c74341be36c70603
ChuHieu <hieu0311@gmail.com>
2020-11-12 15:15:01 +0700

    Add config to call api version 1

--- src/api/index.js
+++ src/api/index.js
@@ -2,19 +2,29 @@ import { ApiConstant, AppConstant } from "../const";
 import apisauce from "apisauce";
 import Cookie from "js-cookie";
 
-export const API_CONFIG_REG = {
+export const defaultConfigV2 = {
   baseURL: ApiConstant.BASE_API,
   headers: ApiConstant.HEADER_DEFAULT,
   timeout: ApiConstant.TIMEOUT,
 };
 
-export const createApiWithToken = (initConfig = API_CONFIG_REG, token) => {
+export const defaultConfigV1 = {
+  baseURL: ApiConstant.BASE_API_V1,
+  headers: ApiConstant.HEADER_DEFAULT,
+  timeout: ApiConstant.TIMEOUT,
+};
+
+export const createApiWithToken = (initConfig = defaultConfigV2, token) => {
   if (!token) token = Cookie.get(AppConstant.KEY_TOKEN);
 
   if (token) {
-    initConfig.headers.Authorization = `Bearer ${token}`;
+    if (ApiConstant.BASE_URL === initConfig.baseURL) {
+      initConfig.headers.Authorization = `Bearer ${token}`;
+    } else {
+      initConfig.headers.Authorization = token;
+    }
   }
   return apisauce.create(initConfig);
 };
 
-export const createApi = () => createApiWithToken(API_CONFIG_REG);
+export const createApi = (initConfig = defaultConfigV2, token) => createApiWithToken(initConfig, token);
